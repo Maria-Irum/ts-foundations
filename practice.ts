6ece37fa9001688d96a89edd3bd3d8fcf707be9f
Maria Irum <maria.irum@buggcy.com>
2026-05-13 13:24:14 -0700

    add typed function for day1

--- practice.ts
+++ practice.ts
@@ -1,3 +1,11 @@
-const name: string = "Hello TypeScript";
-console.log(name);
+const message: string = "Hello TypeScript";
+console.log(message);
 console.log("Working on feature branch");
+
+function greetUser(name: string): string {
+  return `Hello, ${name}!`;
+}
+
+function addNumbers(a: number, b: number): number {
+  return a + b;
+}
